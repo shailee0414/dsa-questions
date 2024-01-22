6bdebfd26db43b9c875ba714f4ed3ee477224571
Shailee Yadav <shaileeyadav130@gmail.com>
2024-01-23 00:00:24 +0530

    another solution

--- MedianOfTwoSortedArr.js
+++ MedianOfTwoSortedArr.js
@@ -2,7 +2,7 @@
 
 // The overall run time complexity should be O(log (m+n)).
 
- 
+
 
 // Example 1:
 
@@ -14,7 +14,7 @@
 // Input: nums1 = [1,2], nums2 = [3,4]
 // Output: 2.50000
 // Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
- 
+
 
 // Constraints:
 
@@ -40,12 +40,53 @@ var findMedianSortedArrays = function (nums1, nums2) {
         return a - b;
     }
     const final = merged.sort(compare)
-     let midIndex = Math.floor(final?.length /2)
+    let midIndex = Math.floor(final?.length / 2)
     if (final?.length % 2 == 0) {
-       
-        return (final[midIndex] +  final[midIndex-1])/2
+
+        return (final[midIndex] + final[midIndex - 1]) / 2
     }
-    else{
-       return  final[midIndex]
+    else {
+        return final[midIndex]
     }
 };
+
+
+// ======= ANOTHER SOLUTION ====
+
+var findMedianSortedArrays = function (nums1, nums2) {
+
+    let final = []
+    let i = 0;
+    let j = 0;
+
+    while (i < nums1.length || j < nums2.length) {
+        if (nums1[i] <= nums2[j] && i < nums1.length && j < nums2.length) {
+            final.push(nums1[i])
+            i++;
+        }
+        else if (nums1[i] >= nums2[j] && j < nums2.length && i < nums1.length) {
+            final.push(nums2[j])
+            j++;
+        }
+        else if (i < nums1.length && j >= nums2.length) {
+            final.push(nums1[i])
+            i++;
+        }
+        else if (j < nums2.length && i >= nums1.length) {
+            final.push(nums2[j])
+            j++
+        }
+    }
+    let midIndex = Math.floor(final?.length / 2)
+    if (final?.length % 2 == 0) {
+
+        return (final[midIndex] + final[midIndex - 1]) / 2
+    }
+    else {
+        return final[midIndex]
+    }
+};
+
+
+
+
